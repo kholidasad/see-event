3980a17438fa4e58f3a94ddde384525489ee61d6
martinmaruli <95682676+martinmaruli@users.noreply.github.com>
2022-01-24 10:33:26 +0700

    +basic feature register/login

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const { User } = require('../models')
+const { verifyToken } = require('../utils/jwt')
+
+module.exports = {
+    isLogin: async (req, res, next) => {
+        try {
+            let token = req.header("Authorization").replace("Bearer ","")
+            // cek token
+            if (!token) {
+                return res.status(401).json({
+                    msg: "Please log in.",
+                    status: "Unauthorized",
+                })
+            }
+            const decoded = verifyToken(token, process.env.JWT_KEY)
+
+            const user = await User.findByPk(decoded.id)
+            if (!user) {
+                return res.status(401).json({
+                    msg: "User not found.",
+                    status: "Unauthorized",
+                })
+            }
+            res.status(200).json({
+                msg: "Successfully logged in.",
+                status: "Success",
+            })
+            next()
+        }
+        catch (error) {
+            res.status(500).json({
+            status: "Internal server error",
+            message: error.message,})
+        }
+    }
+}
